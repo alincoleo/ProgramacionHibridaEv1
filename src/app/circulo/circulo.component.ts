32089268a4bdb20a7e94638d54e36ae54986a827
alincoleo <alvaro.lincoleo@gmail.com>
2024-10-19 14:19:34 -0300

    feature v1.0.4: se crea la logica de circulo, hubo complicaciones en el tamaño de la imagen, se soluciono con css media, además se modifico desde index.html el nombre de la pestaña

--- src/app/circulo/circulo.component.ts
+++ src/app/circulo/circulo.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { IonCard, IonCardHeader, IonCardTitle, IonCardSubtitle, IonCardContent, IonList, IonItem, IonLabel,IonThumbnail, IonImg, IonInput, IonButton } from "@ionic/angular/standalone";
+import { Circulo } from '../modelo/figurasGeometricas';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-circulo',
   templateUrl: './circulo.component.html',
   styleUrls: ['./circulo.component.scss'],
   standalone: true,
+  imports: [IonButton, IonInput, IonImg, IonLabel, IonItem, IonList, IonCardContent, IonCardSubtitle, IonCardTitle, IonCardHeader, IonCard,IonThumbnail,FormsModule],
+
 })
 export class CirculoComponent  implements OnInit {
-
+  radiostr: string=""
+  resultado: string=""
   constructor() { }
-
   ngOnInit() {}
+
+calcularPerimetro() {
+  const radio = parseInt(this.radiostr)
+  const circulo = new Circulo(radio)
+  const perimetro = circulo.calcularPerimetro()
+  //se encontro el methodo toFixed para ajustar a decimales especificos
+  this.resultado=`el perimetro es ${perimetro.toFixed(3)} cm`
+
+}
+
 
 }
